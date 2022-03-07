b64ba2a2e7471578d0382dfe5df6f54f3bd4524e
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2022-03-07 15:41:59 +0530

    added trees max depth code

---
+++ JavaScript/Trees/MaxDepth-Easy.js
@@ -0,0 +1,20 @@
+//// Problem Statement
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+// Input: root = [1,null,2]
+// Output: 2
+
+var maxDepth = function(root) {
+
+    if (!root) return 0;
+
+    let maxLeft = maxDepth(root.left);
+    let maxRight = maxDepth(root.right);
+
+    return Math.max(maxLeft, maxRight) + 1;
+};
